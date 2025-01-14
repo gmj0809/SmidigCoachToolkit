91ac4ff92ce1a0a4cd9a3f3eca3721ff22bd3594
gmj0809 <gisle.mjaatvedt@gmail.com>
2025-01-14 09:29:13 +0100

    La til fil fra python-apps repo

---
+++ scrumMaster.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+
+sprin_start = datetime(2025, 1, 8)
+sprint_length_days = 14
+
+Sprint_end = sprin_start + timedelta(days=sprint_length_days)
+
+today = datetime.now()
+days_remaining = (Sprint_end - today).days
+
+if days_remaining > 0:
+    print(f"There are {days_remaining} days left in the sprint!")
+else:
+    print("The sprint has ended. Timefor a retrospective")
+    
