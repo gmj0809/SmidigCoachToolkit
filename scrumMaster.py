d6e41b7c612be3881c447589c3af8ccfe87b69d4
gmj0809 <gisle.mjaatvedt@gmail.com>
2025-01-14 10:04:59 +0100

    justert-startdato

--- scrumMaster.py
+++ scrumMaster.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 
-sprin_start = datetime(2025, 1, 8)
+sprin_start = datetime(2025, 1, 6)
 sprint_length_days = 14
 
 Sprint_end = sprin_start + timedelta(days=sprint_length_days)
@@ -9,7 +9,7 @@ today = datetime.now()
 days_remaining = (Sprint_end - today).days
 
 if days_remaining > 0:
-    print(f"There are {days_remaining} days left in the sprint NewBie!")
+    print(f"There are {days_remaining} days left in the sprint!")
 else:
     print("The sprint has ended. Timefor a retrospective")
     
